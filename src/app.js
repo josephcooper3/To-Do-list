1f346e8e58b358ff3073adccb3562881e8960bb7
Joe Cooper <josephcooper3@gmail.com>
2019-09-09 21:44:03 +0100

    list displays with title

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      todos: ["Unload dishwasher", "Load laundry", "Put away shopping"],
+    },
+    // methods: {
+
+    // },
+  });
+});
